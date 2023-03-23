995df716f55c4a81fa501024c0c86f695056c78d
kishorejuniper <127823151+kishorejuniper@users.noreply.github.com>
2023-03-23 15:31:38 +0530

    Delete Release/22.4 directory

--- Release/22.4/cn2_central_ansible/destroy_cluster.sh
+++
@@ -1,5 +0,0 @@
-for node in centraljumphost centralctrl centralworker1 centralworker2; do
-virsh destroy $node
-virsh undefine $node
-rm -f /var/lib/libvirt/images/$node.qcow2
-done
--- Release/22.4/cn2_ds1_ansible/destroy_cluster.sh
+++
@@ -1,5 +0,0 @@
-for node in ds1jumphost ds1ctrl ds1worker1 ds1worker2; do
-virsh destroy $node
-virsh undefine $node
-rm -f /var/lib/libvirt/images/$node.qcow2
-done
