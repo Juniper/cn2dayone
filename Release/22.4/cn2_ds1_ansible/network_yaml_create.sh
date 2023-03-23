5915bee763b5bc3fe285c0922d74b49347bec9b1
kishorejuniper <127823151+kishorejuniper@users.noreply.github.com>
2023-03-23 15:35:29 +0530

    Add files via upload

---
+++ Release/22.4/cn2_ds1_ansible/network_yaml_create.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Script to generate network.yaml file for the nodes being configured.
+node_list=( ds1jumphost ds1ctrl ds1worker1 ds1worker2 )
+
+node_gw=$(/sbin/ip route | awk '/default/ { print $3 }')
+env_dns=66.129.233.81
+for node in ${node_list[@]}; do
+node_ip=$( host $node | awk '/has address/ { print $4 }')
+cat > $(pwd)/$node.yaml <<FILE
+ network:
+  ethernets: 
+    enp1s0:
+      dhcp4: no
+      dhcp6: no
+      addresses: [$node_ip/26]
+      gateway4:  $node_gw
+      nameservers: 
+        addresses: [$env_dns]
+  version: 2
+FILE
+done
